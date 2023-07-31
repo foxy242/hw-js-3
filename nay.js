1c66a5318c810bf04d3d8b6d81453e5ec3b36488
foxy242 <114094487+foxy242@users.noreply.github.com>
2023-07-31 21:14:11 +0300

    1

---
+++ nay.js
@@ -0,0 +1,51 @@
+// завдання 1
+
+const pricePerItem = 3500;
+
+const orderedQuantity = 4;
+
+const totalPrice = pricePerItem * orderedQuantity;
+
+console.log(totalPrice);
+
+// завдання 2
+const age = prompt("Скільки вам років?");
+
+const passed = age > 18;
+
+console.log(passed);
+
+// завдання 3
+
+const SAVED_PASSWORD = "123";
+
+const password = prompt("ведіть пароль");
+
+const isMatch = SAVED_PASSWORD === password;
+console.log(isMatch);
+
+
+ // завдання 4
+
+ 
+let a = 5;
+
+a += 2
+
+let b = 10;
+
+b -= 4
+
+let c = 15;
+ 
+c *= 3
+
+let d = 20;
+
+d /= 10
+
+ 
+
+ 
+
+ 
